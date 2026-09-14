4977de320af53d6dec9e2683997212a99761a4e2
ASiKS-Engineering <info@asiks-engineering.com>
2026-09-14 21:54:20 +0200

    Some fixes

---
+++ java/com/example/campernavigator/BootReceiver.kt
@@ -0,0 +1,22 @@
+package com.example.campernavigator
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action != Intent.ACTION_BOOT_COMPLETED &&
+            intent.action != Intent.ACTION_LOCKED_BOOT_COMPLETED
+        ) {
+            return
+        }
+
+        val launchIntent = Intent(context, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        context.startActivity(launchIntent)
+    }
+}
